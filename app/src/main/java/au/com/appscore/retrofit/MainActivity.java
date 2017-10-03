61ffc0d0155323e8fea4fc75172c88e6166260a8
krrish458 <saikrishna1345@gmail.com>
2017-10-03 22:11:31 +1100

    Update MainActivity.java

--- app/src/main/java/au/com/appscore/retrofit/MainActivity.java
+++ app/src/main/java/au/com/appscore/retrofit/MainActivity.java
@@ -16,40 +16,15 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity  {
-    static final String baseurl = "http://emcast.appscorehosting.com.au/";
-    private static final String TAG="MainActivity";
+      private static Retrofit retrofit = null;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //making object of RestAdapter
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(baseurl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(new OkHttpClient())
-                .build();
-        RetrofitPodcatsApi service = retrofit.create(RetrofitPodcatsApi.class);
-
-        Call<Data> call = service.getdata();
-        Log.d(TAG,"afterCall");
-        call.enqueue(new Callback<Data>() {
-
-
-            @Override
-            public void onResponse(Call<Data> call, Response<Data> response) {
-
-                for (Podcasts p:response.body().getData()){
-                    Log.d(TAG,p.toString());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Data> call, Throwable t) {
-
-            }
-        });
+    public static Retrofit getClient(String baseUrl) {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
     }
 }
